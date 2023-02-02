4d2b3e5be15f378926b5df60494c7ee2d552dbd5
vboxuser <vboxuser@WINDA10>
2023-02-02 20:12:17 +0500

    Добавьте файлы проекта.

---
+++ Part1/Part1.cpp
@@ -0,0 +1,49 @@
+﻿// Part1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    const int N = 6;
+    int min, max;
+    int randomDigits[N] = {};
+    srand(time(NULL));
+    for (int i = 0; i < N; i++)
+    {
+        randomDigits[i] = rand(); 
+        cout << randomDigits[i] << endl;
+    }
+
+
+    min = randomDigits[0];
+
+    max = randomDigits[0];
+
+
+    for (int i = 0; i < N; i++)
+    {
+        if (randomDigits[i] > max)
+        {
+            max = randomDigits[i];
+        }
+        if (randomDigits[i] < min)
+        {
+            min = randomDigits[i];
+        }
+
+    }
+    cout << "min:" << min << ", max:" << max;
+
+
+
+
+
+
+    return 0;
+
+
+}
+
---
+++ part1.2/part1.2.cpp
@@ -0,0 +1,40 @@
+﻿// part1.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+    setlocale(0, "");
+    const int N = 6;
+    int sum=0,K, a;
+    int randomDigits[N] = {};
+    cout << "Введите желаемый диапазон чисел от 0 до  \n";
+    cin >> a;
+    cout << "Ваши случайные числа из вашего диапазона следующие : \n";
+    srand(time(NULL));
+    for (int i = 0; i < N; i++)
+    {
+        randomDigits[i] = rand() % a; // рандомные числа от 0 до 50
+        cout << randomDigits[i] << endl;
+    }
+
+    cout << "Введите любое число от 0 до " << a << "\n" ;
+    
+    cin >> K;
+
+    for (int i = 0; i < N; i++)
+    {
+        if (randomDigits[i] < K)
+        {
+            sum += randomDigits[i];
+        }
+       
+
+    }
+
+    cout << "Сумма чисел, которые меньше заданного вами числа = " << sum;
+}
+
